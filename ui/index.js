5738b25d2ad30cf627fdcfaa0179a11bc85c5a99
LorkiVG <lorkiofficial@gmail.com>
2024-07-03 20:07:20 +0300

    Update

--- ui/index.js
+++ ui/index.js
@@ -1,48 +1,118 @@
-const simbattleUnitvalWrps = document.querySelectorAll('.simbattle-unitval-wrp');
-
-function onSimbattleUnitvalOnInput(event)
+function checkSimbattleUnitval(simbattleUnitval) 
 {
-    const simbattleUnitval = event.target;
     const simbattleUnitvalWrp = simbattleUnitval.parentElement.parentElement;
-
     const simbattleUnit = simbattleUnitvalWrp.querySelector(".simbattle-unit");
 
-    if(parseInt(simbattleUnitval.value) <= 0 || simbattleUnitval.value.length == 0)
+    if(simbattleUnit)
     {
-        simbattleUnit.classList.add('-type-empty');
-    }
-    else
-    {
-        simbattleUnit.classList.remove('-type-empty');
+        if (parseInt(simbattleUnitval.value) <= 0 || simbattleUnitval.value.length === 0)
+        {
+            simbattleUnit.classList.add('-type-empty');
+        }
+        else 
+        {
+            simbattleUnit.classList.remove('-type-empty');
+        }
     }
 }
-function onSimbattleUnitvalOnKeypress(event) 
+
+function onSpinboxInput(event) 
+{
+    const simbattleUnitval = event.target;
+    checkSimbattleUnitval(simbattleUnitval);
+}
+
+function onSpinboxKeypress(event) 
 {
     const key = event.key;
-    if(!/[0-9.]/.test(key))
+    if (!/[0-9.]/.test(key)) 
     {
         event.preventDefault();
     }
 }
 
-function onSimbattleLessOnClick(event)
-{
-    const lessElement = event.target;
-}
-
-function onSimbattleMoreOnClick(event)
+function onTypeMoreClick(event) 
 {
     const moreElement = event.target;
+    const spinboxWrp = moreElement.parentElement;
+    const spinbox = spinboxWrp.querySelector(".spinbox");
+    let spinboxValue = parseInt(spinbox.value);
+
+    if (spinbox.value.length === 0) 
+    {
+        spinbox.value = 1;
+    } 
+    else 
+    {
+        spinbox.value = spinboxValue + 1;
+    }
+
+    checkSimbattleUnitval(spinbox);
 }
 
-simbattleUnitvalWrps.forEach((simbattleUnitvalWrp) => 
+function onTypeLessClick(event) 
 {
-    const simbattleUnitval = simbattleUnitvalWrp.querySelector('.simbattle-unitval');
-    const simbattleLess = simbattleUnitvalWrp.querySelector('.-type-less');
-    const simbattleMore = simbattleUnitvalWrp.querySelector('.-type-more');
+    const lessElement = event.target;
+    const spinboxWrp = lessElement.parentElement;
+    const spinbox = spinboxWrp.querySelector(".spinbox");
+    let spinboxValue = parseInt(spinbox.value);
 
-    simbattleUnitval.addEventListener("input", onSimbattleUnitvalOnInput);
-    simbattleUnitval.addEventListener('keypress', onSimbattleUnitvalOnKeypress);
-    simbattleLess.addEventListener("click", onSimbattleLessOnClick);
-    simbattleMore.addEventListener("click", onSimbattleMoreOnClick);
+    if (spinbox.value.length === 0 || spinboxValue == 1) 
+    {
+        spinbox.value = "";
+    } 
+    else 
+    {
+        spinbox.value = spinboxValue - 1;
+    }
+
+    checkSimbattleUnitval(spinbox);
+}
+
+
+
+const spinboxWrpElements = document.querySelectorAll('.spinbox-wrp');
+
+spinboxWrpElements.forEach((spinboxWrp) =>
+{
+    const spinbox = spinboxWrp.querySelector(".spinbox");
+    const typeMore = spinboxWrp.querySelector(".-type-more");
+    const typeLess = spinboxWrp.querySelector(".-type-less");
+    
+    spinbox.addEventListener("input", onSpinboxInput);
+    spinbox.addEventListener('keypress', onSpinboxKeypress);
+
+    typeMore.addEventListener("click", onTypeMoreClick);
+    typeLess.addEventListener("click", onTypeLessClick);
+});
+
+const simbattleResetArmy = document.querySelector('.simbattle-resetArmy');
+
+simbattleResetArmy.addEventListener("click", () =>
+{
+    spinboxWrpElements.forEach((spinboxWrp) =>
+    {
+        const spinbox = spinboxWrp.querySelector(".spinbox");
+        spinbox.value = "";
+
+        checkSimbattleUnitval(spinbox);
+    });
+});
+
+const reportBattleView = document.querySelector('.report-battleView');
+const simbattleSimhead = document.querySelector('.simbattle-simhead');
+const simbattleArmyWrp = document.querySelector('.simbattle-army-wrp');
+
+reportBattleView.addEventListener("change", () => 
+{
+    if(reportBattleView.checked)
+    {
+        simbattleSimhead.classList.remove('reverse');
+        simbattleArmyWrp.classList.remove('reverse');
+    }
+    else
+    {
+        simbattleSimhead.classList.add('reverse');
+        simbattleArmyWrp.classList.add('reverse');
+    }
 });
