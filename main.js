f2c3de5ad2a043eefb28cd947efbfaa282a9e911
LorkiVG <lorkiofficial@gmail.com>
2024-07-17 05:41:48 +0300

    Update

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 960,
+        height: 755,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => 
+{
+    if (process.platform !== 'darwin') 
+    {
+        app.quit();
+    }
+});
+
+app.on('activate', () => 
+{
+    if (BrowserWindow.getAllWindows().length === 0) 
+    {
+        createWindow();
+    }
+});
